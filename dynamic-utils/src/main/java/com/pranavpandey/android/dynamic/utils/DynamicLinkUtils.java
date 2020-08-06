b666963aa8f967d4d8e3e0541d8e8249539d9004
Pranav Pandey <pranavpande92@gmail.com>
2020-08-06 19:26:38 +0530

    Add method to check email client

--- dynamic-utils/src/main/java/com/pranavpandey/android/dynamic/utils/DynamicLinkUtils.java
+++ dynamic-utils/src/main/java/com/pranavpandey/android/dynamic/utils/DynamicLinkUtils.java
@@ -16,6 +16,7 @@
 
 package com.pranavpandey.android.dynamic.utils;
 
+import android.annotation.TargetApi;
 import android.app.ApplicationErrorReport;
 import android.content.ActivityNotFoundException;
 import android.content.ClipData;
@@ -23,26 +24,19 @@ import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Build;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.util.List;
+
 /**
  * A collection of functions to perform various operations on the URL or to generate intents.
  */
 public class DynamicLinkUtils {
-
-    /**
-     * Facebook web url.
-     */
-    private static final String URL_FACEBOOK = "https://www.facebook";
-
-    /**
-     * Facebook app url template to open link in the app.
-     */
-    private static final String URL_FACEBOOK_APP = "fb://facewebmodal/f?href=";
 
     /**
      * Android Market app url template to open app details on older devices.
@@ -98,6 +92,7 @@ public class DynamicLinkUtils {
 
         if (clipboard != null) {
             clipboard.setPrimaryClip(ClipData.newPlainText(label, text));
+
             return true;
         }
 
@@ -124,9 +119,9 @@ public class DynamicLinkUtils {
      */
     public static void share(@NonNull Context context, @Nullable String title,
             @Nullable String message, @Nullable Uri uri, @Nullable String mimeType) {
-        Intent sendIntent = new Intent();
-        sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.setType("text/plain");
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setType("text/plain");
 
         if (title == null) {
             title = context.getResources().getString(R.string.adu_share_to);
@@ -138,17 +133,17 @@ public class DynamicLinkUtils {
                     context.getPackageName());
         }
 
-        sendIntent.putExtra(Intent.EXTRA_TEXT, message);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
 
         if (uri != null) {
-            sendIntent.putExtra(Intent.EXTRA_STREAM, uri);
-            sendIntent.setType(mimeType != null ? mimeType : "*/*");
-            sendIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION
+            intent.putExtra(Intent.EXTRA_STREAM, uri);
+            intent.setType(mimeType != null ? mimeType : "*/*");
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION
                     | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
         }
 
         try {
-            context.startActivity(Intent.createChooser(sendIntent, title));
+            context.startActivity(Intent.createChooser(intent, title));
         } catch (Exception ignored) {
         }
     }
@@ -214,6 +209,42 @@ public class DynamicLinkUtils {
     }
 
     /**
+     * View any URL in the available app or browser. Some URLs will automatically open in their
+     * respective apps if installed on the the device. Special treatment is applied for the
+     * Facebook URLs to open them directly in the app.
+     *
+     * <p><p>This method throws {@link ActivityNotFoundException} if there was no activity found
+     * to run the given intent.
+     *
+     * @param context The context to retrieve the resources.
+     * @param url The web or app link to open.
+     *
+     * @throws ActivityNotFoundException If no activity is found.
+     *
+     * @see Intent#ACTION_VIEW
+     */
+    @TargetApi(Build.VERSION_CODES.R)
+    public static void viewUrl(@NonNull Context context, @NonNull String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        try {
+            if (DynamicSdkUtils.is30()) {
+                intent.addFlags(Intent.FLAG_ACTIVITY_REQUIRE_NON_BROWSER);
+            }
+
+            context.startActivity(intent);
+        } catch (Exception e) {
+            intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            intent.addCategory(Intent.CATEGORY_BROWSABLE);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            context.startActivity(intent);
+        }
+    }
+
+    /**
      * View app on Google Play or Android Market.
      *
      * @param context The context to retrieve the resources.
@@ -223,13 +254,12 @@ public class DynamicLinkUtils {
      *
      * @see Intent#ACTION_VIEW
      */
+    @TargetApi(Build.VERSION_CODES.R)
     public static void viewInGooglePlay(@NonNull Context context, @NonNull String packageName) {
         try {
-            context.startActivity(new Intent(Intent.ACTION_VIEW,
-                    Uri.parse(URL_MARKET + packageName)));
+            viewUrl(context, URL_MARKET + packageName);
         } catch (Exception e) {
-            context.startActivity(new Intent(Intent.ACTION_VIEW,
-                    Uri.parse(URL_PLAY_STORE + packageName)));
+            viewUrl(context, URL_PLAY_STORE + packageName);
         }
     }
 
@@ -246,6 +276,7 @@ public class DynamicLinkUtils {
      *
      * @see #viewInGooglePlay(Context, String)
      */
+    @TargetApi(Build.VERSION_CODES.R)
     public static void rateApp(@NonNull Context context) {
         viewInGooglePlay(context, context.getPackageName());
     }
@@ -263,42 +294,12 @@ public class DynamicLinkUtils {
      *
      * @see Intent#ACTION_VIEW
      */
+    @TargetApi(Build.VERSION_CODES.R)
     public static void moreApps(@NonNull Context context, @NonNull String publisher) {
         try {
-            context.startActivity(new Intent(Intent.ACTION_VIEW,
-                    Uri.parse(URL_MARKET_SEARCH_PUB + publisher)));
+            viewUrl(context, URL_MARKET_SEARCH_PUB + publisher);
         } catch (Exception e) {
-            context.startActivity(new Intent(Intent.ACTION_VIEW,
-                    Uri.parse(URL_GOOGLE_PLAY_SEARCH_PUB + publisher)));
-        }
-    }
-
-    /**
-     * View any URL in the available app or browser. Some URLs will automatically open in their
-     * respective apps if installed on the the device. Special treatment is applied for the
-     * Facebook URLs to open them directly in the app.
-     *
-     * <p><p>This method throws {@link ActivityNotFoundException} if there was no activity found
-     * to run the given intent.
-     *
-     * @param context The context to retrieve the resources.
-     * @param url The web or app link to open.
-     *
-     * @throws ActivityNotFoundException If no activity is found.
-     *
-     * @see Intent#ACTION_VIEW
-     * @see #URL_FACEBOOK_APP
-     */
-    public static void viewUrl(@NonNull Context context, @NonNull String url) {
-        if (url.contains(URL_FACEBOOK)) {
-            try {
-                context.startActivity(new Intent(Intent.ACTION_VIEW,
-                        Uri.parse(URL_FACEBOOK_APP + url)));
-            } catch (Exception e) {
-                context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
-            }
-        } else {
-            context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+            viewUrl(context, URL_GOOGLE_PLAY_SEARCH_PUB + publisher);
         }
     }
 
@@ -324,8 +325,10 @@ public class DynamicLinkUtils {
             @Nullable String subject, @Nullable String text) {
         try {
             Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse(MAIL_TO + email));
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             intent.putExtra(Intent.EXTRA_SUBJECT, subject);
             intent.putExtra(Intent.EXTRA_TEXT, text);
+
             context.startActivity(intent);
         } catch (Exception ignored) {
         }
@@ -366,6 +369,26 @@ public class DynamicLinkUtils {
                     context.getResources().getString(R.string.adu_bug_desc));
         } catch (Exception ignored) {
         }
+    }
+
+    /**
+     * Checks whether the email client exists on the device.
+     *
+     * @param context The context to get the package manager.
+     *
+     * @return {@code true} if the email client exists on the device.
+     */
+    public static boolean isEmailExists(@NonNull Context context) {
+        List<ResolveInfo> list = null;
+
+        try {
+            Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse(MAIL_TO));
+            PackageManager packageManager = context.getPackageManager();
+            list = packageManager.queryIntentActivities(intent, 0);
+        } catch (Exception ignored) {
+        }
+
+        return list != null && list.size() > 0;
     }
 
     /**
@@ -439,18 +462,17 @@ public class DynamicLinkUtils {
             report.crashInfo = crashInfo;
         }
 
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra(Intent.EXTRA_BUG_REPORT, report);
+        if (isGMSExists(context)) {
+            intent.setClassName(PACKAGE_GMS, PACKAGE_GMS + ".feedback.FeedbackActivity");
+        } else {
+            intent.setClassName(PACKAGE_FEEDBACK, PACKAGE_FEEDBACK + ".FeedbackActivity");
+        }
+
         try {
-            if (isGMSExists(context)) {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setClassName(PACKAGE_GMS, PACKAGE_GMS + ".feedback.FeedbackActivity");
-                intent.putExtra(Intent.EXTRA_BUG_REPORT, report);
-                context.startActivity(intent);
-            } else {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setClassName(PACKAGE_FEEDBACK, PACKAGE_FEEDBACK + ".FeedbackActivity");
-                intent.putExtra(Intent.EXTRA_BUG_REPORT, report);
-                context.startActivity(intent);
-            }
+            context.startActivity(intent);
         } catch (Exception e) {
             report(context, appName, email);
         }

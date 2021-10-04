88a74eb175bb281d2bba9f254bf9f1fc21af00da
Pranav Pandey <pranavpande92@gmail.com>
2021-10-04 07:24:30 +0530

    Update device utils
    Add support for Wi-Fi and camera features.

--- dynamic-utils/src/main/java/com/pranavpandey/android/dynamic/util/DynamicDeviceUtils.java
+++ dynamic-utils/src/main/java/com/pranavpandey/android/dynamic/util/DynamicDeviceUtils.java
@@ -22,6 +22,9 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.net.wifi.WifiNetworkSuggestion;
 import android.os.Build;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
@@ -33,12 +36,200 @@ import androidx.core.content.ContextCompat;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import java.util.regex.Pattern;
 
 /**
  * Helper class to detect device specific features like Telephony, etc.
  */
 public class DynamicDeviceUtils {
+
+    /**
+     * Pattern for the WEP encryption.
+     */
+    public static final Pattern PATTERN_WEP = Pattern.compile(
+            "WEP", Pattern.CASE_INSENSITIVE);
+
+    /**
+     * Pattern for the WPA encryption.
+     */
+    public static final Pattern PATTERN_WPA = Pattern.compile(
+            "WPA", Pattern.CASE_INSENSITIVE);
+
+    /**
+     * Returns the Wi-Fi configuration according to the supplied parameters.
+     *
+     * @param ssid The network ssid to be used.
+     * @param password The password to be used.
+     * @param encryption The encryption method to be used.
+     * @param hidden {@code true} if the network is hidden.
+     *
+     * @see WifiConfiguration
+     */
+    @SuppressWarnings("deprecation")
+    public static @Nullable WifiConfiguration getWifiConfiguration(@Nullable String ssid,
+            @Nullable String password, @Nullable String encryption, boolean hidden) {
+        if (DynamicSdkUtils.is29() || ssid == null) {
+            return null;
+        }
+
+        WifiConfiguration config = new WifiConfiguration();
+        config.allowedAuthAlgorithms.clear();
+        config.allowedGroupCiphers.clear();
+        config.allowedKeyManagement.clear();
+        config.allowedPairwiseCiphers.clear();
+        config.allowedProtocols.clear();
+
+        config.SSID = "\"" + ssid + "\"";
+        config.hiddenSSID = hidden;
+
+        if (encryption != null && PATTERN_WEP.matcher(encryption).find()) {
+            config.wepKeys[0]= "\"" + password + "\"";
+            config.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.SHARED);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104);
+            config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+            config.wepTxKeyIndex = 0;
+        } else if (encryption != null && PATTERN_WPA.matcher(encryption).find()) {
+            config.preSharedKey = "\"" + password + "\"";
+            config.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+            config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+            config.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+            config.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+            config.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
+            config.status = WifiConfiguration.Status.ENABLED;
+        } else {
+            config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+        }
+
+        return config;
+    }
+
+    /**
+     * Try to save the Wi-Fi configuration according to the supplied parameters.
+     *
+     * @param ssid The network ssid to be used.
+     * @param password The password to be used.
+     * @param encryption The encryption method to be used.
+     * @param hidden {@code true} if the network is hidden.
+     *
+     * @see #getWifiConfiguration(String, String, String, boolean)
+     * @see WifiManager#addNetworkSuggestions(List)
+     * @see WifiManager#enableNetwork(int, boolean)
+     */
+    @SuppressWarnings("deprecation")
+    @TargetApi(Build.VERSION_CODES.Q)
+    @RequiresPermission(allOf = { Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.CHANGE_WIFI_STATE })
+    public static boolean connectWifi(@Nullable Context context, @Nullable String ssid,
+            @Nullable String password, @Nullable String encryption, boolean hidden) {
+        if (context == null || ssid == null) {
+            return false;
+        }
+
+        WifiManager wifiManager = (WifiManager) context.getApplicationContext()
+                .getSystemService(Context.WIFI_SERVICE);
+
+        if (wifiManager == null) {
+            return false;
+        }
+
+        if (DynamicSdkUtils.is29()) {
+            List<WifiNetworkSuggestion> suggestionsList = new ArrayList<>();
+
+            if (encryption != null && PATTERN_WPA.matcher(encryption).find() && password != null) {
+                WifiNetworkSuggestion wpa2 = new WifiNetworkSuggestion.Builder()
+                        .setSsid(ssid).setWpa2Passphrase(password)
+                        .setIsHiddenSsid(hidden).build();
+                WifiNetworkSuggestion wpa3 = new WifiNetworkSuggestion.Builder()
+                        .setSsid(ssid).setWpa3Passphrase(password)
+                        .setIsHiddenSsid(hidden).build();
+
+                suggestionsList.add(wpa2);
+                suggestionsList.add(wpa3);
+            } else {
+                WifiNetworkSuggestion open =
+                        new WifiNetworkSuggestion.Builder()
+                                .setSsid(ssid)
+                                .setIsHiddenSsid(hidden)
+                                .build();
+
+                suggestionsList.add(open);
+            }
+
+            wifiManager.addNetworkSuggestions(suggestionsList);
+
+            return true;
+        }
+
+        WifiConfiguration configuration;
+        if ((configuration = getWifiConfiguration(ssid, password, encryption, hidden)) != null) {
+            if (!wifiManager.isWifiEnabled()){
+                wifiManager.setWifiEnabled(true);
+            }
+
+            int networkId;
+            if ((networkId = wifiManager.addNetwork(configuration)) != -1) {
+                wifiManager.saveConfiguration();
+
+                if (wifiManager.getConnectionInfo() != null) {
+                    if (wifiManager.getConnectionInfo().getNetworkId() != networkId) {
+                        wifiManager.disableNetwork(wifiManager.getConnectionInfo().getNetworkId());
+                        wifiManager.disconnect();
+
+                        return wifiManager.enableNetwork(networkId, true);
+                    } else {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Checks whether the device has camera feature.
+     *
+     * @param context The context to be used.
+     *
+     * @return {@code true} if the device has camera feature.
+     *
+     * @see PackageManager#FEATURE_CAMERA
+     * @see PackageManager#FEATURE_CAMERA_ANY
+     */
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    public static boolean hasCameraFeature(@Nullable Context context) {
+        if (context == null) {
+            return false;
+        }
+
+        return context.getPackageManager().hasSystemFeature(DynamicSdkUtils.is17()
+                ? PackageManager.FEATURE_CAMERA_ANY : PackageManager.FEATURE_CAMERA);
+    }
+
+    /**
+     * Checks whether the device has flashlight feature.
+     *
+     * @param context The context to be used.
+     *
+     * @return {@code true} if the device has flashlight feature.
+     *
+     * @see PackageManager#FEATURE_CAMERA_FLASH
+     */
+    public static boolean hasFlashlightFeature(@Nullable Context context) {
+        if (context == null) {
+            return false;
+        }
+
+        return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+    }
 
     /**
      * Vibrate device for the supplied duration.
@@ -126,7 +317,11 @@ public class DynamicDeviceUtils {
      *
      * @return {@code true} if the device has MIUI services installed.
      */
-    public static boolean isXiaomiMIUI(@NonNull Context context) {
+    public static boolean isXiaomiMIUI(@Nullable Context context) {
+        if (context == null) {
+            return false;
+        }
+
         return DynamicIntentUtils.isActivityResolved(context,
                 new Intent("miui.intent.action.OP_AUTO_START")
                         .addCategory(Intent.CATEGORY_DEFAULT))
